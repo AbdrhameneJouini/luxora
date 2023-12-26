0db86b579404606a6e317da531ca990d4a289e30
Abdrahemene <abdrahemenejouini@gmail.com>
2023-12-26 15:32:25 +0100

    fixes

--- src/main/java/com/luxora/servlets/GetArticlePage.java
+++ src/main/java/com/luxora/servlets/GetArticlePage.java
@@ -8,7 +8,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "GetArticleDetails", value = "/GetArticleDetails")
+@WebServlet(name = "GetArticlePage", value = "/GetArticlePage")
 public class GetArticlePage extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
